0f8bcbe80181e5fbcd55e8fb11c3a13568ce0507
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-15 12:23:38 +0100

    Update dois.php, index.php, and 2 more files...

---
+++ tres.php
@@ -0,0 +1,9 @@
+<?php
+$txt1 = "Eu sou o joao e";
+$txt2 = "na turma sou o numero";
+$txt3 = "Vou fazer este modulo com";
+$Num1 = "15";
+$Num2 = "20";
+echo $txt1. " " .$txt2. " " .$Num1. ". " .$txt3. " " .$Num2. "Valores";
+?> 
+
---
+++ um.php
@@ -0,0 +1,11 @@
+<?php 
+echo "Guns N' Roses";
+?>
+
+<?php
+echo "C:\\xampp\\";
+?>
+
+<?php
+echo"\"Ser poeta é ser mais alto...\"";
+?>
