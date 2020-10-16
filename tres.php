91b7d8230e4a82c59c65377f73b0603ef71d2080
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-16 10:07:10 +0100

    Update index.php, indexx.php, and tres.php

--- tres.php
+++ tres.php
@@ -1,9 +1,30 @@
 <?php
-$txt1 = "Eu sou o joao e";
+$txt1 = "Eu sou o João e";
 $txt2 = "na turma sou o numero";
 $txt3 = "Vou fazer este modulo com";
 $Num1 = "15";
 $Num2 = "20";
 echo $txt1. " " .$txt2. " " .$Num1. ". " .$txt3. " " .$Num2. "Valores";
-?> 
 
+
+
+echo "<br>Rui Pedro nº10 12-i2";
+
+
+echo "<br><br>Os testes estão no nosso disco de rede em" ;
+echo " \\Curso\\Disciplinas\\Modulos\\Testes\\";
+echo "<br> Em PHP, a declaração de uma variável implica a precedência de um carácter especial: o carater $.";
+echo "<br><br>O professor chegou perto do aluno e exclamou: \"se não estudares, vais reprovar de ano!\".";
+
+
+
+echo "<br><br>O professor
+<br>chegou perto
+<br>do aluno e
+<br>exclamou: \"se não
+<br>estudares, vais
+<br>reprovar de ano!\"";
+
+
+
+?>
